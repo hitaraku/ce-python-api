48a1651e8106ff8c5b16f4f79580598d24c10bd5
tNakajima <airu85@live.jp>
2018-01-28 10:42:42 +0900

    firstCommit

---
+++ src/getMarket.py
@@ -0,0 +1,26 @@
+import urllib.request
+import json
+
+# マーケット ID
+summary = json.loads(urllib.request.urlopen("https://www.coinexchange.io/api/v1/getmarketsummaries").read())
+firstMarketID = summary['result'][0]['MarketID']
+summaryVol = len(summary['result'])
+
+# マーケットデータ
+market = json.loads(urllib.request.urlopen("https://www.coinexchange.io/api/v1/getmarkets").read())
+
+# マーケット概要
+summary = json.loads(urllib.request.urlopen("https://www.coinexchange.io/api/v1/getmarketsummaries").read())
+
+for i in range(summaryVol - int(firstMarketID)):
+    MarketNumber = int(firstMarketID) + i - 1
+    print(
+        "ticker: ", market['result'][MarketNumber]['MarketAssetCode'], market['result'][MarketNumber]['BaseCurrencyCode'], 
+        "LastPrice: ", summary['result'][MarketNumber]['LastPrice'], 
+        "HighPrice: ", summary['result'][MarketNumber]['HighPrice'], 
+        "LowPrice: ", summary['result'][MarketNumber]['LowPrice'], 
+        "Volume: ", summary['result'][MarketNumber]['Volume']
+        )
+
+
+
